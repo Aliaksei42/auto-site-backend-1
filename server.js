761a30aa4cd9f13a84f415805fd8e473d44a6b99
Aliaksei Iuko <alexivko01@gmail.com>
2024-01-24 13:19:46 +0100

    Fixed bags

--- server.js
+++ server.js
@@ -9,24 +9,6 @@ const postRoutes = require('./routes/post-routes')
 
 const PORT = process.env.PORT || 1000;
 const URL = process.env.MONGODB_URL;
-// const PORT = process.env.PORT || 1000
-// const URL =
-//   'mongodb+srv://alex:Passs321@cluster0.tq2r9lo.mongodb.net/autoSite?retryWrites=true&w=majority'
-// const URL = 'mongodb://localhost:27017/autoSite'
-
-// const corsOptions = {
-//   origin: 'https://auto-site-frontend-git-master-alexivko01-gmailcom.vercel.app/',
-//   optionsSuccessStatus: 200,
-//   credentials: true,
-// }
-
-// const corsOptions = {
-//   origin: 'https://auto-site-frontend-git-master-alexivko01-gmailcom.vercel.app/',
-//   optionsSuccessStatus: 200,
-//   credentials: true,
-// }
-
-// app.use(cors(corsOptions));
 
 const app = express()
 app.use(cors())
